15af2291c6da62d10909dc8431cda5ee72bbdc1b
user <kavinnikov@gmail.com>
2022-12-01 10:13:10 +0300

    rrrr

--- shop/settings.py
+++ shop/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
+#tttt
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
