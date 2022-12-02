facfe9ad76c07f996009ecf02181a777f30bba1d
user <kavinnikov@gmail.com>
2022-12-02 10:53:02 +0300

    tyeuei

--- shop/urls.py
+++ shop/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('', include('myshop.urls', namespace='shop')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
